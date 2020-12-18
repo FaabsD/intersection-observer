b7439b551406d4c1d20331e0b1c3db3302c4d5b1
Fabian <19959@ma-web.nl>
2020-12-18 15:27:47 +0100

    creeër observer en log de sections

--- script/interSectionObserver.js
+++ script/interSectionObserver.js
@@ -1,4 +1,18 @@
 let allLinks = document.querySelectorAll('.nav-item__link');
+let sections = document.querySelectorAll('section');
+
+let options = {}
+
+const handelCut = (entries, observer) => {
+    entries.forEach(entry => {
+        console.log(entry.target + " doorsnijdt " + entry.isIntersecting);
+    })
+
+};
+
+let observer = new IntersectionObserver(handelCut, options);
+
+observer.observe(sections[1]);
 
 // functie die de class active verwijderd uit een menu item
 const deleteActive = () => {
